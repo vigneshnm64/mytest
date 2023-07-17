23ad9678cce07e5d2aee38a14961751c98d3d85b
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-17 13:49:57 +0530

    new-colour

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
       <h3>Welcome to furgal sintific by vignesh and by anil and all</h3>
       <h2 style={{color:'red'}}>we are free to express our self to the world </h2>
       <h2 style={{color:'blue'}}>we are free to express our self to the world </h2>
-      <h2>we are free to express our self to the world </h2>
+      <h2 style={{color:'green'}} >we are free to express our self to the world </h2>
       <h2>we are free to express our self to the world </h2>
     </div>
   )
