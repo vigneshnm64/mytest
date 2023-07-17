5839aaf9bf622853e096f9d1ca415bd517a30c7b
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-17 10:49:52 +0530

    newmessage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ const App = () => {
     <div>
       <h3>Welcome to furgal sintific by vignesh and by anil and all</h3>
       <h2>we are free to express our self to the world </h2>
+      <h2>we are free to express our self to the world </h2>
+      
     </div>
   )
 }
