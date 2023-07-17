8554bfa729171f89ba0a63929ac42aa1bcb5365c
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-17 13:36:34 +0530

    new-commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div>
       <h3>Welcome to furgal sintific by vignesh and by anil and all</h3>
-      <h2>we are free to express our self to the world </h2>
+      <h2 style={{color:'red'}}>we are free to express our self to the world </h2>
       <h2>we are free to express our self to the world </h2>
       <h2>we are free to express our self to the world </h2>
       <h2>we are free to express our self to the world </h2>
