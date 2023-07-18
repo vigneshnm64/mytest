f6b4aa7bf07366f189e9ad03a795af96e7ea256a
vigneshnm64 <vigneshnm64@gmail.com>
2023-07-18 20:34:39 +0530

    newmessage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
       <h2 style={{color:'red'}}>we are free to express our self to the world </h2>
       <h2 style={{color:'blue'}}>we are free to express our self to the world </h2>
       <h2 style={{color:'green'}} >we are free to express our self to the world </h2>
+      <h2 style={{color:'green'}} >we are free to express our self to the world </h2>
       <h2>we are free to express our self to the world </h2>
     </div>
   )
